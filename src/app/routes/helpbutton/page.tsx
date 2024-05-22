5c9d582d1ed3e33f9617b3acac54275779505975
davidespinosa8 <ddavidespinosa8@gmail.com>
2024-05-22 10:17:23 -0300

    Anulacion de botones link y color de boton

--- src/app/routes/helpbutton/page.tsx
+++ src/app/routes/helpbutton/page.tsx
@@ -4,7 +4,7 @@ import Buttons from "@/components/Buttons/buttons";
 export default function HelpButton() {
   return (
     <div className="my-16">
-      <p className="text-3xl text-center text-white font-mono">Hace clic en cualquiera de nuestros botones y aporta con lo que puedas</p>
+      <p className="text-3xl text-center text-white font-mono py-10">Hace clic en cualquiera de nuestros botones y aporta con lo que puedas</p>
       <Buttons />
     </div>
   )
--- src/components/Buttons/buttons.tsx
+++ src/components/Buttons/buttons.tsx
@@ -6,7 +6,7 @@ export function Buttons() {
       <h1 className="text-center font-Exo py-10 text-5xl text-slate-300">APORTE ÚNICO</h1>
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-center overflow-hidden">
-          <div className="px-6">
+          {/* <div className="px-6">
             <Link href="https://mpago.la/1fzGYty">
               <button className="w-full relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
                 <span className="relative px-10 py-5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-3xl md:text-5xl text-center w-full">
@@ -41,10 +41,10 @@ export function Buttons() {
                 </span>
               </button>
             </Link>
-          </div>
+          </div> */}
           <div className="col-span-1 md:col-span-2 px-5">
             <button className="w-full relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
-              <span className="w-full relative px-10 py-5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-2xl font-Exo">
+              <span className="w-full relative px-10 py-5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0 text-2xl font-Exo">
                 Alias: Competenciamexico
               </span>
             </button>

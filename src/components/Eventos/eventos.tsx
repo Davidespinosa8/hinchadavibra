6b994349025edfb671bd7a17c8c290bf010316c8
davidespinosa8 <ddavidespinosa8@gmail.com>
2024-06-11 09:28:40 -0300

    modificacion en fonts

--- src/components/Eventos/eventos.tsx
+++ src/components/Eventos/eventos.tsx
@@ -12,7 +12,7 @@ export function Eventos(){
             </a>
             <div className="w-2/3 pl-5">
               <h2 className="relative mb-3 text-xl font-bold leading-tight text-white"><a href="#_">Venta de tallarines</a></h2>
-              <p className="text-sm text-white opacity-70">Se realizo Una venta de tallarines con la colaboracion de toda la gente de nuestra querida ciudad, contando con el apoyo de cada una de nuestras familias</p>
+              <p className="text-sm text-white opacity-90">Se realizo Una venta de tallarines con la colaboracion de toda la gente de nuestra querida ciudad, contando con el apoyo de cada una de nuestras familias</p>
               <span className="relative flex mt-3 text-xs text-white opacity-90">
                 <span className="mr-1 font-semibold">Realizada en sede vibra</span>·<span className="ml-1">Marzo, 2024</span>
               </span>
@@ -50,7 +50,7 @@ export function Eventos(){
             </a>        
             <div className="w-2/3 pl-5">        
               <h2 className="relative mb-3 text-xl font-bold leading-tight text-white"><a href="#_">Bingo</a></h2>          
-              <p className="text-sm text-white opacity-50">Nuestro último evento un bingo familiar, en donde disfrutamos de una hermosa tarde acompañadas de todos los participantes.</p>          
+              <p className="text-sm text-white opacity-90">Nuestro último evento un bingo familiar, en donde disfrutamos de una hermosa tarde acompañadas de todos los participantes.</p>          
               <span className="relative flex mt-3 text-xs text-white opacity-90">          
                 <span className="mr-1 font-semibold">Sede Club San Martin</span>·<span className="ml-1">Mayo, 2024</span>              
               </span>          
@@ -62,7 +62,7 @@ export function Eventos(){
             </a>        
             <div className="w-2/3 pl-5">        
               <h2 className="relative mb-3 text-xl font-bold leading-tight text-white"><a href="#_">Pastelitos</a></h2>          
-              <p className="text-sm text-white opacity-50">Para el 25 de Mayo como la fecha lo pide hicimos pastelitos de Batata y membrillo, siendo un rotundo éxito. El trabajo y la colaboración se hicieron notar.</p>          
+              <p className="text-sm text-white opacity-90">Para el 25 de Mayo como la fecha lo pide hicimos pastelitos de Batata y membrillo, siendo un rotundo éxito. El trabajo y la colaboración se hicieron notar.</p>          
               <span className="relative flex mt-3 text-xs text-white opacity-90">          
                 <span className="mr-1 font-semibold">Sede Casa de Day</span>·<span className="ml-1">Mayo, 2024</span>              
               </span>          

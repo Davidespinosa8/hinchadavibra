37c69e0c4696a7dc4b16aeee30bac8aa68e29f20
davidespinosa8 <ddavidespinosa8@gmail.com>
2024-06-13 21:21:08 -0300

    Sponsors

--- src/components/Sponsor/sponsor.tsx
+++ src/components/Sponsor/sponsor.tsx
@@ -28,7 +28,19 @@ export default function Sponsor() {
         <img width="80%" src="/sponsors/polark/PolarK.gif" className="mb-10" alt="Sponsor GIF 7"/>
       </div>
       <div className="w-full flex justify-center">
-        <img width="65%" src="/sponsors/placerurbano/Placerurbano.gif" className="mb-10" alt="Sponsor GIF 5"/>
+        <img width="65%" src="/sponsors/placerurbano/Placerurbano.gif" className="mb-10" alt="Sponsor GIF 8"/>
+      </div>
+      <div className="w-full flex justify-center">
+        <img width="80%" src="/sponsors/alcon/alcon.gif" className="mb-10" alt="Sponsor GIF 9"/>
+      </div>
+      <div className="w-full flex justify-center">
+        <img width="80%" src="/sponsors/marinamartoreli/marinamartorelli.gif" className="mb-10" alt="Sponsor GIF 10"/>
+      </div>
+      <div className="w-full flex justify-center">
+        <img width="55%" src="/sponsors/midebilidad/Midebilidad.gif" className="mb-10" alt="Sponsor GIF 11"/>
+      </div>
+      <div className="w-full flex justify-center">
+        <img width="80%" src="/sponsors/lavie/Lavie.gif" className="mb-10" alt="Sponsor GIF 12"/>
       </div>
       <div className="container pt-20 pb-40 text-center">
         <p className="text-xl font-Mulish text-blue-400">Esta pagina esta administrada por la comisión de padres <q>Hinchada oficial de Vibra</q> queremos invitar a las empresas que gusten participar de este maravilloso sueño. Quienes gusten pueden comunicarse por los medios aportados en nuestro contacto agradecemos su tiempo</p>
